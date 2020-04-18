e98be9f0662df0785472e42057aedf7b22e45e2e
Moeh-Jama <mohamed.jama@ucdconnect.ie>
2020-04-18 18:48:30 +0100

    added initial starting point of application, along with argParser for parsing arguemets such as # of mlps, etc

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from argparse import ArgumentParser
+
+def main():
+
+
+if __name__ == '__main__':
+	# method for adding arguements
+	# parser = ArgumentParser()
+	# parser.add_arguement("")
+	main()
